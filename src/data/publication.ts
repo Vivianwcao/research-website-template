8e155c1a4f45211f1725a7b79209f9be7c163332
Vivian <59620790+Vivianwcao@users.noreply.github.com>
2025-04-07 00:01:54 -0700

    update tailwind

--- src/data/publication.ts
+++ src/data/publication.ts
@@ -12,19 +12,19 @@ export interface Publication {
 }
 
 export const publicationData: Publication[] = [
-	// If you don't want to show publications, just make the array empty.
-	{
-		// year: "2024",
-		// conference: "NeurIPS",
-		title: "Scalable Causal Discovery in High-Dimensional Time Series",
-		// authors: "Jane Smith, Sarah Johnson, Yue Zhang",
-		// paperUrl: "https://arxiv.org/abs/2409.15476",
-		// codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
-		//bibtex: "https://arxiv.org/abs/2409.15476.bib",
-		// tldr: "Using causal discovery to find the causal structure of high-dimensional time series data.",
-		imageUrl: "https://i.imgur.com/OfQNkxj.png",
-		// award: "🏆 Best Paper Award",
-		// if you have an image in public/images, you can use it like this:
-		// imageUrl: "/images/publication-image.jpg"
-	},
+	// // If you don't want to show publications, just make the array empty.
+	// {
+	// 	// year: "2024",
+	// 	// conference: "NeurIPS",
+	// 	title: "Me making pizza",
+	// 	// authors: "Jane Smith, Sarah Johnson, Yue Zhang",
+	// 	// paperUrl: "https://arxiv.org/abs/2409.15476",
+	// 	// codeUrl: "https://github.com/jsmith/scalable-causal-discovery",
+	// 	//bibtex: "https://arxiv.org/abs/2409.15476.bib",
+	// 	// tldr: "Using causal discovery to find the causal structure of high-dimensional time series data.",
+	// 	imageUrl: "https://i.imgur.com/OfQNkxj.png",
+	// 	// award: "🏆 Best Paper Award",
+	// 	// if you have an image in public/images, you can use it like this:
+	// 	// imageUrl: "/images/publication-image.jpg"
+	// },
 ];

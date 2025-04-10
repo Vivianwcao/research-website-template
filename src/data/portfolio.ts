e1b30423acaa9aca57a369dc5f8f8a635b9e4c67
Vivian <59620790+Vivianwcao@users.noreply.github.com>
2025-04-10 11:28:39 -0700

    modify projects

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -14,10 +14,13 @@ export const portfolioData: Portfolio[] = [
 		description:
 			"Blackjack Royale is an online multiplayer Blackjack game where players can log in with Gmail or Outlook account, and either start a new game or join an existing one on the lobby page. Multiple games can run at the same time in separate rooms, allowing different groups to play without interfering with each other. Firestore database handles real-time updates, making the gameplay smooth and interactive.",
 		technologies: [
-			"React",
+			"React.js",
+			"Vite",
+			"Axios",
 			"Firebase Authentification",
-			"Firestore database",
-			"Deck of cards API",
+			"Firestore real-time database",
+			"Rest API",
+			"CSS",
 			"Sass",
 		],
 		projectUrl: "https://blackjackroyalebyvivian.netlify.app/",
@@ -29,7 +32,15 @@ export const portfolioData: Portfolio[] = [
 		title: "SimplyDone To-do App",
 		description:
 			"A to-do app that lets users add new entries, modify entry titles, and create or update sublists.",
-		technologies: ["React", "MongoDB", "Sass", "Serverless functions"],
+		technologies: [
+			"React.js",
+			"JavaScript",
+			"MongoDB",
+			"Sass",
+			"Node.js",
+			"Express",
+			"Serverless functions",
+		],
 		projectUrl: "https://simplydonebyvivian.vercel.app/",
 		imageUrl: "https://i.imgur.com/rsfyxLC.gif",
 		codeUrl: "https://github.com/Vivianwcao/Mern-Todo-list",
@@ -38,7 +49,7 @@ export const portfolioData: Portfolio[] = [
 		title: "BizBot AI",
 		description:
 			"A 24-hour Microsoft Industry Hackathon team project implemented by team of 6. BizBot AI delivers a crystal-clear AI adoption plan tailored to each business's unique needs, transforming AI complexity into accessible business value for Microsoft users.",
-		technologies: ["React", "Google Gemini Copilot", "Sass"],
+		technologies: ["React.js", "Google Gemini Copilot", "CSS", "Sass"],
 		projectUrl: "https://yvonnelutrinh.github.io/microsoft-hackathon-bizbot/",
 		imageUrl: "https://i.imgur.com/gGsFkuQ.gif",
 		codeUrl: "https://github.com/AAdemide/industry-project-team2",
@@ -47,7 +58,7 @@ export const portfolioData: Portfolio[] = [
 		title: "Snaps",
 		description:
 			"Snaps is a photo-sharing application built with React and Sass, allowing users to upload pictures and leave comments on individual images.",
-		technologies: ["React", "Sass", "RESTful API"],
+		technologies: ["React.js", "Sass", "REST API"],
 		projectUrl: "https://snapsphotogallery.netlify.app/",
 		imageUrl: "https://i.imgur.com/LHoGjxw.gif",
 		codeUrl: "https://snapsphotogallery.netlify.app/",
@@ -56,7 +67,7 @@ export const portfolioData: Portfolio[] = [
 		title: "Bandsite",
 		description:
 			"Bandsite is a prototype website built for a music band using JavaScript and Sass, featuring show schedules, band introductions, and a comment section for users to share their thoughts on the latest album.",
-		technologies: ["Javascript", "HTML5", "Sass", "RESTful API"],
+		technologies: ["Javascript", "HTML5", "Sass", "REST API"],
 		projectUrl: "https://bandsiteconcert.netlify.app/",
 		imageUrl: "https://i.imgur.com/9WsWG4k.gif",
 		codeUrl: "https://bandsiteconcert.netlify.app/",

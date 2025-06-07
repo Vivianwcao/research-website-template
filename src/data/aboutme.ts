7c82bc6814bdcf2ac35f3cf06a848c2a53fd6d6c
Vivian <59620790+Vivianwcao@users.noreply.github.com>
2025-06-07 08:35:34 -0700

    update aboutme

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -32,7 +32,8 @@ export const aboutMe: AboutMe = {
   design patterns, and OS concepts in Python, Java, and C.<br /><br /> Recently,
   I completed the Software Engineering Diploma Program at BrainStation,
   where I focused on building full stack web applications using React, Sass, and MySQL/PostgreSQL.
-  My projects also involved working with non relational databases like Firestore(Firebase) and MongoDB,
+  My projects also involved working with non relational databases like Firestore(Firebase) and
+  <a href="https://deckofcardsapi.com/" target="_blank" style="underline: none">Deck Of Card</a> REST API,
   which you can see in my latest Capstone project
   <a href="https://blackjackroyalebyvivian.netlify.app" target="_blank" style="color: #991194"><i><b>Blackjack Royale</b></i></a>. I also incorporated MongoDB and serverless functions in my mini project
   <a href="https://simplydonebyvivian.vercel.app" target="_blank" style="color: #991194"><i><b>SimplyDone</b></i></a>, a to-do app.<br /><br />
